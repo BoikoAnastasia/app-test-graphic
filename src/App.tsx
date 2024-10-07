7b0664d4c56ceed80d8d6eaea3da0d252919b805
Анастасия Бойко <anastasia151000@rambler.ru>
2024-10-08 01:15:36 +0300

    sample

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { MainBody } from './components/MainBody';
+
+function App() {
+  return (
+    <div className='container'>
+    <MainBody/>
+  </div>
+  )
+}
+
+export default App;
